8f9fe069b96704e40bf04ddeaf5d7d874882caf1
unknown <gunich.alina@gmail.com>
2023-07-03 17:05:01 +0300

    form valid ready, need fics button on page 3

--- script/form.js
+++ script/form.js
@@ -1,23 +1,28 @@
 (function(){
     const Form = {
+        agreeElement: null,
+        processElement: null,
         fields:[
             {
                 name:"name",
                 id: 'name',
                 element: null,
                 regex: /^[А-Я][а-я]+\s*$/,
+                valid: false,
             },
             {
                 name:'lastName',
                 id:'last-name',
                 element: null,
                 regex: /^[А-Я][а-я]+\s*$/,
+                valid: false,
             },
             {
                 name:'email',
                 id: 'email',
                 element: null,
-                regex: /^w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                valid: false,
             }, 
         ],
         init(){
@@ -29,19 +34,34 @@
                 }
             })
 
-            
+            this.processElement = document.getElementById('process')
+            this.processElement.onchange=function(){
+
+            }
+
+            this.agreeElement = document.getElementById('agree');
+            this.agreeElement.onchange = function(){
+                that.validateForm()
+            }
 
         },
         validateField(field,element){
             if(!element.value || !element.value.match(field.regex)){ 
                 element.parentNode.style.borderColor = 'red'
+                field.valid=false
             }else{
                 element.parentNode.removeAttribute('style')
+                field.valid=true
             }
             this.validateForm()
         },
         validateForm(){
-
+            const validForm = this.fields.every(itm => itm.valid)
+            if(this.agreeElement.checked && validForm){
+                this.processElement.removeAttribute('disabled')
+            }else{
+                this.processElement.setAttribute('disabled', 'disabled')
+            }
         }
     };
 
